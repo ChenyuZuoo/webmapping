b9b819944a0896afdd74d92b007d3335edff2788
ChenyuZuoo <zuocy1992@foxmail.com>
2018-06-18 15:52:33 +0200

    'move_geojson_to_external_file'

---
+++ metadata-geojson.js
@@ -0,0 +1,34 @@
+//geojson file
+var geojsonFeature = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9998241,
+                    39.7471494
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 51
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9983545,
+                    39.7502833
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 52
+        }
+    ]
+};

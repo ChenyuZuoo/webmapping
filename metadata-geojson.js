c02dec684ae39b21b13ed155f791027897858af0
ChenyuZuoo <zuocy1992@foxmail.com>
2018-06-22 11:56:09 +0200

    'geojson_template'

--- metadata-geojson.js
+++ metadata-geojson.js
@@ -6,29 +6,162 @@ var geojsonFeature = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    -104.9998241,
-                    39.7471494
+                    121.7471494,
+                    31.2998241
                 ]
             },
             "type": "Feature",
             "properties": {
                 "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
             },
-            "id": 51
+            "id": 11
         },
         {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    -104.9983545,
-                    39.7502833
+                    121.7502833,
+                    31.1983545
                 ]
             },
             "type": "Feature",
             "properties": {
                 "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
             },
-            "id": 52
+            "id": 12
         }
     ]
 };
+
+
+var geojsonPolygons = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                        [
+                            [116.878499726, 27.483689998],
+                            [121.870303722, 27.483689998],
+                            [121.870303722, 34.813542995],
+                            [116.878499726, 34.813542995],
+                            [116.878499726, 27.483689998]
+                        ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "Railway stations",
+                "year": "2013",
+                "scale": "1:250,000",
+                "coordinate": "GCS_Xian_1980",
+                "format": "shapefile",
+                "type": "points",
+                "datasize": "33KB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "'Type' is the rail way station type: 1 for freight station, 2 for passenger station, -1 for unknown. 'rank' is the level of the stations.",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\3 1 250k The distribution and statistical data of the railways in the Yangtze River Delta(2013)"
+            },
+            "id": 3.3
+        },
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [116.878499726, 27.483689998],
+                        [121.870303722, 27.483689998],
+                        [121.870303722, 34.813542995],
+                        [116.878499726, 34.813542995],
+                        [116.878499726, 27.483689998]
+                    ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "Railway lines",
+                "year": "2013",
+                "scale": "1:250,000",
+                "coordinate": "GCS_Xian_1980",
+                "format": "shapefile",
+                "type": "lines",
+                "datasize": "180KB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\3 1 250k The distribution and statistical data of the railways in the Yangtze River Delta(2013)"
+            },
+            "id": 3.2
+        },
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [116.878499726, 27.483689998],
+                        [121.870303722, 27.483689998],
+                        [121.870303722, 34.813542995],
+                        [116.878499726, 34.813542995],
+                        [116.878499726, 27.483689998]
+                    ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "Boundaries",
+                "year": "2013",
+                "scale": "1:250,000",
+                "coordinate": "GCS_Xian_1980",
+                "format": "shapefile",
+                "type": "lines",
+                "datasize": "180KB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "province boundaries",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\3 1 250k The distribution and statistical data of the railways in the Yangtze River Delta(2013)"
+            },
+            "id": 3.2
+        },
+    ]
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

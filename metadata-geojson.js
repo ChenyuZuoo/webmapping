c1b633bce0685363939ff5ac613d6a858825fc2c
ChenyuZuoo <zuocy1992@foxmail.com>
2018-06-25 09:48:24 +0200

    'transportation_geojson'

--- metadata-geojson.js
+++ metadata-geojson.js
@@ -34,7 +34,7 @@ var geojsonFeature = {
 };
 
 
-var geojsonPolygons = {
+var transportation = {
     "type": "FeatureCollection",
     "features": [
         {
@@ -55,7 +55,7 @@ var geojsonPolygons = {
                 "title": "Railway stations",
                 "year": "2013",
                 "scale": "1:250,000",
-                "coordinate": "GCS_Xian_1980",
+                "projection": "WGS84",
                 "format": "shapefile",
                 "type": "points",
                 "datasize": "33KB",
@@ -86,7 +86,7 @@ var geojsonPolygons = {
                 "title": "Railway lines",
                 "year": "2013",
                 "scale": "1:250,000",
-                "coordinate": "GCS_Xian_1980",
+                "projection": "WGS84",
                 "format": "shapefile",
                 "type": "lines",
                 "datasize": "180KB",
@@ -117,7 +117,7 @@ var geojsonPolygons = {
                 "title": "Boundaries",
                 "year": "2013",
                 "scale": "1:250,000",
-                "coordinate": "GCS_Xian_1980",
+                "projection": "WGS84",
                 "format": "shapefile",
                 "type": "lines",
                 "datasize": "180KB",
@@ -128,7 +128,193 @@ var geojsonPolygons = {
                 "description": "province boundaries",
                 "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\3 1 250k The distribution and statistical data of the railways in the Yangtze River Delta(2013)"
             },
-            "id": 3.2
+            "id": 3.1
+        },
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [117.060349107, 28.005108724],
+                        [121.862711672, 28.005108724],
+                        [121.862711672, 34.581657519],
+                        [117.060349107, 34.581657519],
+                        [117.060349107, 28.005108724]
+                    ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "Port distribution",
+                "year": "2013",
+                "scale": "1:250,000",
+                "projection": "WGS84",
+                "format": "shapefile",
+                "type": "points",
+                "datasize": "113KB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "the distribution of the ports and attributes",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\4 1 250k Distribution and statistical data of major ports in the Yangtze River Delta(2013)\\1250k Distribution and statistical data of major ports in the Yangtze River Delta  (2013)\\port"
+            },
+            "id": 4.1
+        },
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [117.060349107, 28.005108724],
+                        [121.862711672, 28.005108724],
+                        [121.862711672, 34.581657519],
+                        [117.060349107, 34.581657519],
+                        [117.060349107, 28.005108724]
+                    ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "Channel distribution",
+                "year": "2013",
+                "scale": "1:250,000",
+                "projection": "WGS84",
+                "format": "shapefile",
+                "type": "lines",
+                "datasize": "10.6KB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "the distribution of the ports and attributes",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\4 1 250k Distribution and statistical data of major ports in the Yangtze River Delta(2013)\\1250k Distribution and statistical data of major ports in the Yangtze River Delta  (2013)\\port"
+            },
+            "id": 4.2
+        },
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [116.373309202601, 30.7676156762115],
+                        [121.900375366211, 30.7676156762115],
+                        [121.900375366211, 35.1244698223977],
+                        [116.373309202601, 35.1244698223977],
+                        [116.373309202601, 30.7676156762115]
+                    ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "road",
+                "year": "1990",
+                "scale": "1:250,000",
+                "projection": "WGS84",
+                "format": "shapefile",
+                "type": "lines",
+                "datasize": "1.59MB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "the distribution of the roads, including national roads, province road, contry road and channels",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\5 1 250k road data in Jiangsu province(1990-2015 years)\\1 250k road data in Jiangsu province (1990-2015 years)"
+            },
+            "id": 5.1
+        },
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [116.356134019324, 30.7610750080186],
+                        [121.900375366211, 30.7610750080186],
+                        [121.900375366211, 35.1162356378226],
+                        [116.356134019324, 35.1162356378226],
+                        [116.356134019324, 30.7610750080186]
+                    ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "road",
+                "year": "1996",
+                "scale": "1:250,000",
+                "projection": "WGS84",
+                "format": "shapefile",
+                "type": "lines",
+                "datasize": "1.59MB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "the distribution of the roads, including national roads, province road, contry road and channels",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\5 1 250k road data in Jiangsu province(1990-2015 years)\\1 250k road data in Jiangsu province (1990-2015 years)"
+            },
+            "id": 5.2
+        },
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [116.356134019324, 30.7610750080186],
+                        [121.900375366211, 30.7610750080186],
+                        [121.900375366211, 35.1162356378226],
+                        [116.356134019324, 35.1162356378226],
+                        [116.356134019324, 30.7610750080186]
+                    ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "road",
+                "year": "2002",
+                "scale": "1:250,000",
+                "projection": "WGS84",
+                "format": "shapefile",
+                "type": "lines",
+                "datasize": "1.59MB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "the distribution of the roads, including national roads, province road, contry road and channels",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\5 1 250k road data in Jiangsu province(1990-2015 years)\\1 250k road data in Jiangsu province (1990-2015 years)"
+            },
+            "id": 5.3
+        },
+        {
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [116.356134019324, 30.7610750080186],
+                        [121.900375366211, 30.7610750080186],
+                        [121.900375366211, 35.1162356378226],
+                        [116.356134019324, 35.1162356378226],
+                        [116.356134019324, 30.7610750080186]
+                    ]
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "road",
+                "year": "2002",
+                "scale": "1:250,000",
+                "projection": "WGS84",
+                "format": "shapefile",
+                "type": "lines",
+                "datasize": "1.59MB",
+                "category": "transportation",
+                "language": "CHE",
+                "constraints": "apply before usage",
+                "contact": "Chenyu Zuo",
+                "description": "the distribution of the roads, including national roads, province road, contry road and channels",
+                "storageLocation": "Y:\\06_Geodata\\2018_Yangtze_Delta_Data\\5 1 250k road data in Jiangsu province(1990-2015 years)\\1 250k road data in Jiangsu province (1990-2015 years)"
+            },
+            "id": 5.4
         },
     ]
 };
